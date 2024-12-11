57d32c3770240c4e7d1ece6a48bdc30d2ade9e6b
oliverog21 <oliver.ojang@gmail.com>
2024-12-11 20:48:52 +0200

    posts

--- server/database.js
+++ server/database.js
@@ -3,18 +3,21 @@ const Pool = require('pg').Pool;
 // this code will work and a table will be created if you have already created the "testWad" database.
 const pool = new Pool({
     user: "postgres",
-    password: "1234", // Enter your password here
-    database: "testWad", //Try to use the same name for your database
+    password: "muskelmees", // Enter your password here
+    database: "testWad", // Try to use the same name for your database
     host: "localhost",
     port: "5433"
 });
 
-
-const execute = async(query1, query2) => {
+const execute = async(query1, mdealol, query2) => {
     try {
         await pool.connect(); // gets connection
-        await pool.query(query1); // sends queries
-        await pool.query(query2); // sends queries
+        await pool.query(query1); // sends first query to create the first table
+        console.log("First table created");
+        await pool.query(mdealol); // inserts initial post into "posttable"
+        console.log("Data inserted into 'posttable'");
+        await pool.query(query2); // sends second query to create the second table
+        console.log("Second table created");
         return true;
     } catch (error) {
         console.error(error.stack);
@@ -30,6 +33,11 @@ const createTblQuery1 = `
         "urllink" VARCHAR(200)  
     );`;
 
+const mdealol = `
+    INSERT INTO "posttable" ("id", "title", "body", "urllink") 
+    VALUES (1, 'test', 'Testest', 'laosldoasld') 
+    ON CONFLICT ("id") DO NOTHING;`; // Prevent duplicate insertion if "id" already exists
+
 const createTblQuery2 = `
     CREATE TABLE IF NOT EXISTS "users" (
         id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -37,12 +45,10 @@ const createTblQuery2 = `
         password VARCHAR(200) NOT NULL 
     );`;
 
-
-
-// A function to execute the previous query   
-execute(createTblQuery1, createTblQuery2).then(result => {
+// A function to execute the previous queries   
+execute(createTblQuery1, mdealol, createTblQuery2).then(result => {
     if (result) {
-        console.log('If does not exists, table "users" and  table "posttable" are created');
+        console.log('If they do not exist, the tables "users" and "posttable" have been created, and data has been added to "posttable".');
     }
 });
 
